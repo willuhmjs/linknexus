1f9e6d48eed344ca826b5855529b3cd60b7278c8
William Faircloth <willuhmjs@gmail.com>
2023-06-01 21:01:59 +0000

    chore: begin models

--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import { MONGO_URI } from "$env/static/private";
 
 // initialize database
@@ -7,3 +7,18 @@ mongoose.connect(MONGO_URI).then(() => {
 }).catch((err) => {
     console.log(err);
 });
+
+export const User = mongoose.model('User', new Schema({
+    email: String,
+    password: String,
+    username: String,
+}, { timestamps: true }))
+
+export const UserData = mongoose.model('UserData', new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    data: Object,
+}, { timestamps: true }))
+
+// example to link user and userdata 
+// const user = await User.findOne({ email: 'test@test' })
+// const userdata = await UserData.findOne({ user: user._id })
