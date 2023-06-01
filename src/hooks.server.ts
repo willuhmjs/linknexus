79bb25db5a3959f5d647cd95338160e930b11685
William Faircloth <willuhmjs@gmail.com>
2023-06-01 17:16:05 +0000

    idk

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,4 @@
-import { config } from 'dotenv';
 import "$lib/mongo";
-config();
 
 export async function handle({ event, resolve }) {
     return await resolve(event);
--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -1,7 +1,9 @@
+import { config } from 'dotenv';
+config();
 import mongoose from 'mongoose';
 
 // initialize database
-mongoose.connect(process.env.MONGO_URI as string).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
     console.log(err);
