efcfc898f79910b2cbf804cec560bda3fa21f2f8
willuhmjs <willuhmjs@gmail.com>
2023-06-04 22:53:39 -0400

    refactor: combine auth into one

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,30 +1,83 @@
-import { redirect } from '@sveltejs/kit';
+import { redirect, fail, type Cookies } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import type { Actions } from './$types';
-import { getUserFromJWT } from '$lib/auth';
+import { getUserFromJWT, login, register } from '$lib/auth';
 
-export const load: PageServerLoad = async ({ cookies }) => {
-  console.log("this loads whenever GLOBAL")
+const checkAuth = async (cookies: Cookies) => {
   const token = cookies.get("session");
   
   if (!token) {
-    throw redirect(303, '/admin/login');
+    return {
+      authorized: false
+    }
   }
 
   const { success, user } = await getUserFromJWT(token);
   if (!success) {
-    throw redirect(303, '/admin/login');
+    return {
+      authorized: false
+    }
+  } else {
+    return {
+      user: JSON.parse(JSON.stringify(user)),
+      authorized: true
+    }
   }
+}
   
-  return {
-    user: JSON.parse(JSON.stringify(user))
-  };
+
+export const load: PageServerLoad = async ({ cookies }) => {
+  console.log("this loads whenever GLOBAL")
+  return checkAuth(cookies);
 };
 
 
 export const actions = {
-    default: async (event) => {
-        console.log("this loads whenever ACTION")
-        // TODO log the user in
+    register: async ({ cookies, request }) => {
+      const data = await request.formData();
+      const username = data.get('username')?.toString();
+      const password = data.get('password')?.toString();
+      const email = data.get('email')?.toString();
+      if (!username || !password || !email) {
+          return fail(403, { success: false })
+      }
+      const { success, token } = await register(email, username, password);
+      
+      if (token) {
+          cookies.set('session', token, {
+              httpOnly: true,
+              maxAge: 60 * 60 * 24 * 7 // 1 week
+          });
+      } else {
+          return fail(403, { success })
+      }
+    },
+
+    login: async ({ cookies, request }) => {
+      const data = await request.formData();
+      const username = data.get('username')?.toString();
+      const password = data.get('password')?.toString();
+      if (!username || !password) {
+          return fail(403, { success: false })
+      }
+  
+      const { success, token } = await login(username, password);
+      
+      if (token) {
+          cookies.set('session', token, {
+              httpOnly: true,
+              maxAge: 60 * 60 * 24 * 7 // 1 week
+          });
+          // redirect to admin page
+      } else {
+          return fail(403, { success })
+      }
+
+
+    },
+
+    special: async ({ cookies, request }) => {
+      
     }
+
 } satisfies Actions;
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -2,13 +2,29 @@
     export let data;
     import Theme from "$lib/theme";
 </script>
-{#if data}
+
+{#if data?.authorized}
     <h1>{data.user.username}</h1>
     <h2>{data.user.email}</h2>
     <p>{data.user.bio || "No bio!"}</p>
     <p>{Theme[data.user.theme]}</p>
+
+    <form method="POST" action="?/special">
+        <input type="submit"/>
+    </form>
+{:else}
+    <h2>Login</h2>
+    <form method="POST" action="?/login">
+        <input type="text" name="username" placeholder="Username" />
+        <input type="password" name="password" placeholder="Password" />
+        <button type="submit">Login</button>
+    </form>
+    <h2>Register</h2>
+    <form method="POST" action="?/register">
+        <input type="text" name="username" placeholder="Username" />
+        <input type="email" name="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" />
+        <button type="submit">Login</button>
+    </form>
 {/if}
 
-<form method="POST">
-    <input type="submit"/>
-</form>
