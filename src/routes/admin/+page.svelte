deac8b2580d2521de123f9b44a28ea39d76a7e01
willuhmjs <willuhmjs@gmail.com>
2023-06-10 11:24:03 -0400

    fix: proper deletion of list elements + no cache

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -3,119 +3,121 @@
 	export let form: ActionData;
 	import Theme from '$lib/theme';
 	import type { ActionData } from './$types.js';
-	import Sortable from 'sortablejs'
+	import Sortable from 'sortablejs';
 	import { onMount } from "svelte";
 	import Auth from '$lib/modules/Auth.svelte';
-
-	let links: HTMLUListElement;
-	let cachedLinks: { title: string, url: string, icon: string }[];
+  
+	let linksElement: HTMLUListElement;
+	let links: { title: string, url: string, icon: string, _id: string }[] = data.user.links;
+  
 	onMount(() => {
-		if (!links) return;
-		cachedLinks = Array.from(data.user.links);
-		Sortable.create(links, {
-			group: {
-				name: 'links',
-				put: true
-			},
-			animation: 200,
-			onUpdate: () => {
-				console.log(cachedLinks)
-			},
-		});
+	  Sortable.create(linksElement, {
+		group: {
+		  name: 'links',
+		  put: true
+		},
+		animation: 200,
+		onUpdate: () => {
+		  links = Array.from(linksElement.querySelectorAll("li")).map((li) => {
+			const linkId = li.querySelector("a")?.id || "";
+			return links.find((link) => link?._id === linkId);
+		  }).filter(Boolean) as { title: string, url: string, icon: string, _id: string }[];
+		},
+	  });
 	});
-
+  
 	function deleteLink(event: Event) {
-		const form = event.target as HTMLFormElement;
-		const formData = new FormData(form);
-		const title = formData.get('title') as string;
-		const linkIndex = cachedLinks.findIndex((link) => link.title === title);
-		cachedLinks.splice(linkIndex, 1);
-		console.log(cachedLinks)
-		const li = links.querySelector(`li:nth-child(${linkIndex + 1})`);
-		li?.remove();
+	  const form = event.target as HTMLFormElement;
+	  const formData = new FormData(form);
+	  const id = formData.get('id') as string;
+	  const linkIndex = links.findIndex((link) => link._id === id);
+	  links.splice(linkIndex, 1);
+	  const li = linksElement.querySelector(`li:nth-child(${linkIndex + 1})`);
+	  li?.remove();
 	}
-</script>
+  </script>
 
 {#if data?.user}
-	<h1>{data.user.username}</h1>
-	<h2>{data.user.email}</h2>
-	<p>{data.user.bio || 'No bio!'}</p>
-	<p>{Theme[data.user.theme]}</p>
-	<form method="POST" action="?/theme">
-		{#if form?.ref === 'theme'}
-			<p style={form?.error ? 'color: red;' : 'color: green;'}>{form?.message}</p>
-		{/if}
-		<select name="theme">
-			{#each Object.keys(Theme).filter((value) => !isNaN(parseFloat(value))) as theme}
-				<option selected={theme == data.user.theme} value={theme}>{Theme[theme]}</option>
-			{/each}
-		</select>
-		<button type="submit">Update Theme</button>
-	</form>
-	<form method="POST" action="?/logout">
-		<button type="submit">Logout</button>
-	</form>
-	<form method="POST" action="?/bio">
-		{#if form?.ref === 'bio'}
-			<p style={form?.error ? 'color: red;' : 'color: green;'}>{form?.message}</p>
-		{/if}
-		<input type="text" name="bio" placeholder={data?.user.bio || ''} autocomplete="off" />
-		<button type="submit">Update Bio</button>
-	</form>
+  <h1>{data.user.username}</h1>
+  <h2>{data.user.email}</h2>
+  <p>{data.user.bio || 'No bio!'}</p>
+  <p>{Theme[data.user.theme]}</p>
+  <form method="POST" action="?/theme">
+    {#if form?.ref === 'theme'}
+      <p style={form?.error ? 'color: red;' : 'color: green;'}>{form?.message}</p>
+    {/if}
+    <select name="theme">
+      {#each Object.keys(Theme).filter((value) => !isNaN(parseFloat(value))) as theme}
+        <option selected={theme == data.user.theme} value={theme}>{Theme[theme]}</option>
+      {/each}
+    </select>
+    <button type="submit">Update Theme</button>
+  </form>
+  <form method="POST" action="?/logout">
+    <button type="submit">Logout</button>
+  </form>
+  <form method="POST" action="?/bio">
+    {#if form?.ref === 'bio'}
+      <p style={form?.error ? 'color: red;' : 'color: green;'}>{form?.message}</p>
+    {/if}
+    <input type="text" name="bio" placeholder={data?.user.bio || ''} autocomplete="off" />
+    <button type="submit">Update Bio</button>
+  </form>
 
-	<!-- add link -->
-	<form method="POST" action="?/link">
-		{#if form?.ref === 'link'}
-			<p style={form?.error ? 'color: red;' : 'color: green;'}>{form?.message}</p>
-		{/if}
-		<input
-			type="text"
-			name="icon"
-			placeholder="Emoji"
-			autocomplete="off"
-			value={form?.icon || '🔗'}
-		/>
-		<input
-			type="text"
-			name="title"
-			placeholder="Title"
-			autocomplete="off"
-			value={form?.title || ''}
-		/>
-		<input type="url" name="url" placeholder="URL" autocomplete="off" value={form?.url || ''} />
-		<button type="submit">Add Link</button>
-	</form>
+  <!-- add link -->
+  <form method="POST" action="?/link">
+    {#if form?.ref === 'link'}
+      <p style={form?.error ? 'color: red;' : 'color: green;'}>{form?.message}</p>
+    {/if}
+    <input
+      type="text"
+      name="icon"
+      placeholder="Emoji"
+      autocomplete="off"
+      value={form?.icon || '🔗'}
+    />
+    <input
+      type="text"
+      name="title"
+      placeholder="Title"
+      autocomplete="off"
+      value={form?.title || ''}
+    />
+    <input type="url" name="url" placeholder="URL" autocomplete="off" value={form?.url || ''} />
+    <button type="submit">Add Link</button>
+  </form>
 
-	<!-- display links -->
-	{#if cachedLinks?.length > 0}
-		<h2>Links</h2>
-		<ul bind:this={links}>
-			{#each cachedLinks as link}
-				<li>
-					<a href={link.url} target="_blank" rel="noopener noreferrer">
-						{link.icon}
-						{link.title}
-					</a>
-					<!-- delete button -->
-					<form on:submit|preventDefault={deleteLink}>
-						<input type="hidden" name="title" value={link.title} />
-						<button type="submit">Delete</button>
-					</form>
-				</li>
-			{/each}
-		</ul>
-	{/if}
+  <!-- display links -->
+  <h2>Links</h2>
+  <ul bind:this={linksElement}>
+    {#if links?.length > 0}
+      {#each links as link (link._id)}
+        <li>
+          <a href={link.url} target="_blank" rel="noopener noreferrer" id={link._id}>
+            {link.icon}
+            {link.title}
+          </a>
+          <!-- delete button -->
+          <form on:submit|preventDefault={deleteLink}>
+            <input type="hidden" name="id" value={link._id} />
+            <button type="submit">Delete</button>
+          </form>
+        </li>
+      {/each}
+    {/if}
+  </ul>
 {:else}
-	<Auth {form} />
+  <Auth {form} />
 {/if}
-<style>
-	ul {
-		width: 50%;
-	}
 
-	li {
-		padding: 0.25rem;
-		background-color: lightgray;
-		margin: 0.25rem;
-	}
+<style>
+  ul {
+    width: 50%;
+  }
+
+  li {
+    padding: 0.25rem;
+    background-color: lightgray;
+    margin: 0.25rem;
+  }
 </style>
