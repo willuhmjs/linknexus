454d1dc570ecc57627956e49dc10294d31481f81
willuhmjs <willuhmjs@gmail.com>
2023-06-12 20:59:45 -0400

    fix: change dask redirect

--- src/routes/dash/+page.server.ts
+++ src/routes/dash/+page.server.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ cookies }) => {
-	throw redirect(303, '/dash/user');
+	throw redirect(303, '/dash/links');
 };
