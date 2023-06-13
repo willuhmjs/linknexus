00b9e8ff7ec14b2ca693247c0e1fc0c0a13957e7
willuhmjs <willuhmjs@gmail.com>
2023-06-12 20:34:07 -0400

    fix: disable root /dash functionality

--- src/routes/dash/+page.server.ts
+++ src/routes/dash/+page.server.ts
@@ -1,37 +1,6 @@
-import { fail, type Cookies } from '@sveltejs/kit';
-import { getUserFromJWT } from '$lib/auth';
-import * as validator from '$lib/validation';
-import type { IUser } from '$lib/mongo';
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
-const checkAuth = async (cookies: Cookies) => {
-	const token = cookies.get('session');
-
-	if (!token) {
-		return null;
-	}
-
-	const user = await getUserFromJWT(token, { password: 0 });
-	return user;
-};
-
-export const actions = {
-	theme: async ({ cookies, request }) => {
-		const user = await checkAuth(cookies);
-		if (!user) {
-			return fail(403, { ref: 'theme', error: true, message: 'Not authorized!' });
-		}
-
-		const data = await request.formData();
-		const theme = parseInt(data.get('theme')?.toString() || '');
-
-		try {
-			validator.theme.parse(theme);
-		} catch (e) {
-			return fail(403, { ref: 'theme', error: true, message: e.errors[0].message });
-		}
-
-		const validUser = user as IUser;
-		validUser.theme = theme;
-		await validUser.save();
-	}
+export const load: PageServerLoad = async ({ cookies }) => {
+	throw redirect(303, '/dash/user');
 };
