f52805d24f582d99568a4b4ca0a9b3c17984b327
willuhmjs <willuhmjs@gmail.com>
2023-06-17 12:28:47 -0400

    fix: schema recompile error

--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -6,43 +6,42 @@ export const connect = async () => {
 	console.log('connected to mongo');
 };
 
-export const User = mongoose.model<IUser>(
-	'User',
-	new Schema(
-		{
-			email: { type: String, required: true, unique: true },
-			password: { type: String, required: true },
-			username: { type: String, required: true, unique: true },
-			bio: { type: String, required: false },
-			theme: {
-				background: {
-					type: { type: Number, required: true, default: 0, enum: BackgroundType },
-					color: { type: String, required: true, default: '#000000' },
-				},
-				button: {
-					radius: { type: Number, required: true, default: 0, min: 0, max: 100 },
-					style: { type: Number, required: true, default: 0, enum: ButtonStyle },
-					// backgroundColor applies to both border and background colors
-					color: { type: String, required: true, default: '#000000' },
-					fontColor: { type: String, required: true, default: '#000000' },
-				},
-				font: { type: Number, required: true, default: 0, enum: Font },
-				// fontColor is overriden by button.fontColor
+const userSchema = new Schema(
+	{
+		email: { type: String, required: true, unique: true },
+		password: { type: String, required: true },
+		username: { type: String, required: true, unique: true },
+		bio: { type: String, required: false },
+		theme: {
+			background: {
+				type: { type: Number, required: true, default: 0, enum: BackgroundType },
+				color: { type: String, required: true, default: '#000000' },
+			},
+			button: {
+				radius: { type: Number, required: true, default: 0, min: 0, max: 100 },
+				style: { type: Number, required: true, default: 0, enum: ButtonStyle },
+				// backgroundColor applies to both border and background colors
+				color: { type: String, required: true, default: '#000000' },
 				fontColor: { type: String, required: true, default: '#000000' },
 			},
-			links: [
-				{
-					title: { type: String, required: true },
-					url: { type: String, required: true },
-				}
-			],
-			specials: [
-				{
-					type: { type: Number, required: true, enum: SpecialLink },
-					username: { type: String, required: true }
-				}
-			]
+			font: { type: Number, required: true, default: 0, enum: Font },
+			// fontColor is overriden by button.fontColor
+			fontColor: { type: String, required: true, default: '#000000' },
 		},
-		{ timestamps: true }
-	)
+		links: [
+			{
+				title: { type: String, required: true },
+				url: { type: String, required: true },
+			}
+		],
+		specials: [
+			{
+				type: { type: Number, required: true, enum: SpecialLink },
+				username: { type: String, required: true }
+			}
+		]
+	},
+	{ timestamps: true }
 );
+
+	export const User = mongoose.model<IUser>('User', userSchema);
