cb9f945e823fa52d410b19ae801fd1d871fdae1d
willuhmjs <willuhmjs@gmail.com>
2023-06-20 09:22:44 -0400

    fix: margin

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 		@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
 
 		body {
-			margin: 1rem;
+			margin: 0.75rem;
 			padding: 0;
 			background-color: #F5F5FA;
 			color: #292727;
