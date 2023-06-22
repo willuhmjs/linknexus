d163c2df0a03adc19ab4e1a5c85dd0eb5653607b
willuhmjs <willuhmjs@gmail.com>
2023-06-21 21:27:25 -0400

    feat: labels, better buttons, better or divider

--- src/routes/admin/links/+page.svelte
+++ src/routes/admin/links/+page.svelte
@@ -6,6 +6,13 @@
 	import LinkEditor from '$lib/modules/LinkEditor.svelte';
 	let links: { title: string; url: string; _id: string }[] = data.user?.links;
 	let specials: { type: SpecialLink; username: string; _id: string }[] = data.user?.specials;
+	
+	const labelProportions = {
+		title: 2,
+		url: 2,
+		type: 1,
+		username: 2,
+	};
 </script>
 
 <div class="addLink">
@@ -13,25 +20,31 @@
 		{#if form?.ref === 'link'}
 			<p class={form?.error ? 'error' : 'success'}>{form?.message}</p>
 		{/if}
-		<input
-			id="title"
-			type="text"
-			name="title"
-			placeholder="Title"
-			autocomplete="off"
-			value={form?.title || ''}
-			required
-		/>
-		<input
-			required
-			id="url"
-			type="url"
-			name="url"
-			placeholder="URL"
-			autocomplete="off"
-			value={form?.url || ''}
-		/>
-		<button type="submit">Add Link</button>
+		<label for="title" style="flex: ${labelProportions.title}">
+			<span>Title</span>
+			<input
+				id="title"
+				type="text"
+				name="title"
+				placeholder="My Awesome Website"
+				autocomplete="off"
+				value={form?.title || ''}
+				required
+			/>
+		</label>
+		<label for="url" style="flex: ${labelProportions.url}">
+			<span>URL</span>
+			<input
+				required
+				id="url"
+				type="url"
+				name="url"
+				placeholder="https://willuhmjs.com"
+				autocomplete="off"
+				value={form?.url || ''}
+			/>
+		</label>
+		<button type="submit"><i class="fa-solid fa-arrow-up-right-from-square"></i>Add Hyperlink</button>
 	</form>
 	<hr />
 	<div class="or-divider">OR</div>
@@ -40,15 +53,21 @@
 		{#if form?.ref === 'special'}
 			<p class={form?.error ? 'error' : 'success'}>{form?.message}</p>
 		{/if}
-		<select name="type" required>
-			{#each Object.keys(SpecialLink).filter((value) => !isNaN(parseFloat(value))) as type}
-				<option value={type}>
-					{SpecialLink[parseInt(type)]}
-				</option>
-			{/each}
-		</select>
-		<input required type="text" name="username" placeholder="username" autocomplete="off" />
-		<button type="submit">Add Link</button>
+		<label for="type" style="flex: ${labelProportions.type}">
+			<span>Type</span>
+			<select name="type" required>
+				{#each Object.keys(SpecialLink).filter((value) => !isNaN(parseFloat(value))) as type}
+					<option value={type}>
+						{SpecialLink[parseInt(type)]}
+					</option>
+				{/each}
+			</select>
+		</label>
+		<label for="username" style="flex: ${labelProportions.username}">
+			<span>Username</span>
+			<input required type="text" name="username" placeholder="willuhmjs" autocomplete="off" />
+		</label>
+		<button type="submit"><i class="fa-solid fa-user-plus"></i>Add Social Link</button>
 	</form>
 </div>
 <LinkEditor {links} />
@@ -65,15 +84,22 @@
 	select,
 	input[type='text'],
 	input[type='url'] {
-		flex: 1;
 		min-width: 0;
 	}
 
+	hr:first-of-type {
+		margin-top: 1.2rem;
+	}
+
+	hr:last-of-type {
+		margin-bottom: 1.2rem;
+	}
+
 	.or-divider {
+		margin: 0.25rem 0;
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		margin: 0.25rem;
 		color: #999;
 		font-weight: bold;
 	}
@@ -82,6 +108,21 @@
 		margin: 0.5rem 0;
 		display: flex;
 		gap: 0.25rem;
+	}
+
+	label {
+		display: flex;
+		flex-direction: column;
+		flex: 1;
+	}
+
+	label > span {
+		margin-bottom: 0.5rem;
+		font-size: 0.85rem;
+	}
+
+	button > i {
+		margin-right: 10px;
 	}
 
 	form input,
@@ -98,5 +139,7 @@
 		border: none;
 		border-radius: 4px;
 		cursor: pointer;
+		margin-top: auto;
 	}
+
 </style>
