abdb12545c2e240d0ec8f7ad9e345cc28cd68eb5
willuhmjs <willuhmjs@gmail.com>
2023-07-13 13:30:36 -0400

    feat: better error

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,24 +1,26 @@
 import { checkAuth } from '$lib/auth';
 import { connect } from '$lib/mongo';
-import { redirect, type Handle, type RequestEvent } from '@sveltejs/kit';
+import { redirect, type Handle, error } from '@sveltejs/kit';
 await connect();
 
 const requestCounter = new Map();
 const maxReqs = 100;
 
-setInterval(() => requestCounter.clear(), 60 * 1000);
+setInterval(() => {
+	requestCounter.clear();
+}, 60 * 1000);
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const request = event.request;
 	// todo make this better
 	const ip = request.headers.get('cf-connecting-ip') || request.headers.get('x-real-ip') || request.headers.get('x-forwarded-for') || event.getClientAddress();
 	if (!ip) {
-		return new Response("Invalid request. IP address is missing", { status: 400 })
+		throw error(400, 'Invalid request. IP address is missing.')
 	}
 
 	const count = requestCounter.set(ip, (requestCounter.get(ip) || 0) + 1).get(ip);
 	if (count > maxReqs) {
-		return new Response("Too many requests. Please try again later.", { status: 429 })
+		throw error(429, 'Too many requests.');
 	}
 
 	if (event.url.pathname.startsWith('/admin')) {
