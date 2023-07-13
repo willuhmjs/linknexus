805a5ec67b0ac74048fdac1614ec94808c61bb82
willuhmjs <willuhmjs@gmail.com>
2023-07-13 00:15:36 -0400

    chore: disable csrf in dev mode

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -12,10 +11,12 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+		csrf: {
+			checkOrigin: process.env.NODE_ENV === 'production'
+		},
 		alias: {
 			$config: './config.ts'
 		}
 	}
 };
-
 export default config;
