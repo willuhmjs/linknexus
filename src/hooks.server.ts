c16d7cf23d8220e08712f5fcc376625cb0396da8
willuhmjs <willuhmjs@gmail.com>
2023-07-13 00:15:42 -0400

    feat: bad ip ratelimiting

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,26 @@
 import { checkAuth } from '$lib/auth';
 import { connect } from '$lib/mongo';
-import { redirect } from '@sveltejs/kit';
+import { redirect, type Handle, type RequestEvent } from '@sveltejs/kit';
 await connect();
 
-export async function handle({ event, resolve }) {
+const requestCounter = new Map();
+const maxReqs = 5;
+
+setInterval(() => requestCounter.clear(), 60 * 1000);
+
+export const handle: Handle = async ({ event, resolve }) => {
+	const request = event.request;
+	// todo make this better
+	const ip = request.headers.get('cf-connecting-ip') || request.headers.get('x-real-ip') || request.headers.get('x-forwarded-for') || event.getClientAddress();
+	if (!ip) {
+		return new Response("Invalid request. IP address is missing", { status: 400 })
+	}
+
+	const count = requestCounter.set(ip, (requestCounter.get(ip) || 0) + 1).get(ip);
+	if (count > maxReqs) {
+		return new Response("Too many requests. Please try again later.", { status: 429 })
+	}
+
 	if (event.url.pathname.startsWith('/admin')) {
 		const user = await checkAuth(event.cookies);
 		if (!user) {
@@ -12,5 +29,6 @@ export async function handle({ event, resolve }) {
 		}
 		event.locals.user = user;
 	}
+
 	return await resolve(event);
 }
