8d7827c40b9e01f96e2e5697f18df984cbdce1b8
willuhmjs <willuhmjs@gmail.com>
2023-07-31 17:43:37 -0400

    fix: sass mode fixes

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 // edit this file to change the default values for the server
 // import variables from config as needed
-import { MONGO_URI, JWT_KEY } from '$env/static/private';
+import { MONGO_URI, JWT_KEY, SAAS_RAW } from '$env/static/private';
 export { MONGO_URI, JWT_KEY };
-export const SAAS = true;
+export const SAAS = SAAS_RAW === "false" ? false : true;
