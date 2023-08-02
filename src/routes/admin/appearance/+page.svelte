1a8ca8ebb48dfe438f2a6d809fe3fd5b11f6a5b3
willuhmjs <willuhmjs@gmail.com>
2023-08-01 22:29:47 -0400

    feat: SUPER EPIC CATEGORIESIISIS

--- src/routes/admin/appearance/+page.svelte
+++ src/routes/admin/appearance/+page.svelte
@@ -5,7 +5,6 @@
 	import wuser from '$lib/user';
 	import api from '$lib/api';
 	import { onMount } from 'svelte';
-	// on:submit|preventDefault={async (e) => (data = await api('/admin/appearance/theme', e))}
 
 	onMount(() => {
 		let form: HTMLFormElement = document.getElementById('form') as HTMLFormElement;
@@ -23,38 +22,43 @@
 		<p class="error">{data?.message}</p>
 	{/if}
 	<h2>Background</h2>
-	<p>Background Color</p>
-	<input type="color" name="backgroundColor" value={$wuser.theme.background.color} required />
+	<div class="category">
+		<h4>Type</h4>
+		<select name="backgroundType" required>
+			{#each Object.keys(BackgroundType).filter((value) => !isNaN(parseFloat(value))) as type}
+				<option selected={type == $wuser.theme.background.type.toString()} value={type}
+					>{BackgroundType[parseInt(type)]}</option
+				>
+			{/each}
+		</select>
 
-	<p>Background Type</p>
-	<select name="backgroundType" required>
-		{#each Object.keys(BackgroundType).filter((value) => !isNaN(parseFloat(value))) as type}
-			<option selected={type == $wuser.theme.background.type.toString()} value={type}
-				>{BackgroundType[parseInt(type)]}</option
-			>
-		{/each}
-	</select>
+		<h4>Color</h4>
+		<input type="color" name="backgroundColor" value={$wuser.theme.background.color} required />
+	</div>
+	
 	<h2>Button</h2>
+	<div class="category">
+		<h4>Border</h4>
+		<select name="buttonStyle" required>
+			{#each Object.keys(ButtonStyle).filter((value) => !isNaN(parseFloat(value))) as style}
+				<option selected={style == $wuser.theme.button.style.toString()} value={style}
+					>{ButtonStyle[parseInt(style)]}</option
+				>
+			{/each}
+		</select>
+		<h4>Border Color</h4>
+		<input type="color" name="buttonBorderColor" value={$wuser.theme.button.borderColor} required />
 
-	<p>Button Border</p>
-	<select name="buttonStyle" required>
-		{#each Object.keys(ButtonStyle).filter((value) => !isNaN(parseFloat(value))) as style}
-			<option selected={style == $wuser.theme.button.style.toString()} value={style}
-				>{ButtonStyle[parseInt(style)]}</option
-			>
-		{/each}
-	</select>
+		<h4>Background Color</h4>
+		<input type="color" name="buttonColor" value={$wuser.theme.button.color} required />
 
-	<p>Button Border Color</p>
-	<input type="color" name="buttonBorderColor" value={$wuser.theme.button.borderColor} required />
+		<h4>Font Color</h4>
+		<input type="color" name="buttonFontColor" value={$wuser.theme.button.fontColor} required />
+	</div>
 
-	<p>Button Color</p>
-	<input type="color" name="buttonColor" value={$wuser.theme.button.color} required />
-
-	<p>Button Font Color</p>
-	<input type="color" name="buttonFontColor" value={$wuser.theme.button.fontColor} required />
 	<h2>General</h2>
-	<p>Page Font</p>
+	<div class="category">
+	<h4>Page Font</h4>
 	<select name="font" required>
 		{#each Object.keys(Font).filter((value) => !isNaN(parseFloat(value))) as font}
 			<option selected={font == $wuser.theme.font.toString()} value={font}
@@ -62,6 +66,24 @@
 			>
 		{/each}
 	</select>
-	<p>Page Font Color</p>
+	<h4>Page Font Color</h4>
 	<input type="color" name="fontColor" value={$wuser.theme.fontColor} required />
+</div>
 </form>
+
+<style>
+	.category {
+		padding: 0.5rem;
+		background-color: #fafafa;
+		border: 2px solid #e8e8ed;
+		border-radius: 0.5rem;
+	}
+
+	.category h4:first-child {
+		margin-top: 5px;
+	}
+
+	.category h4 {
+		margin-bottom: 0.7rem;
+	}
+</style>
