8806e012e613fe8f42d20602f8dd322c7b7a1d0f
willuhmjs <willuhmjs@gmail.com>
2024-03-03 17:51:36 -0500

    fix: saas check bug

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,10 +3,11 @@ import { SAAS } from '$config';
 import { getFirstUser } from '$lib/auth';
 import { redirect } from '@sveltejs/kit';
 export const load = (async () => {
-	if (SAAS) throw redirect(307, '/admin');
-	if (!SAAS) {
+	const user = await getFirstUser();
+	if (SAAS || !user) throw redirect(307, '/admin');
+	if (!SAAS && user) {
 		return {
-			user: JSON.parse(JSON.stringify(await getFirstUser()))
+			user: JSON.parse(JSON.stringify(user))
 		};
 	}
 }) satisfies PageServerLoad;
